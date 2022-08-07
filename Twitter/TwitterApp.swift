0f356f4eb3c1ed7236d2827a76bdfc37081a1552
Victor Han <liyanghan1112@gmail.com>
2022-08-07 09:51:06 -0500

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Victor Han on 2022/8/7.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
