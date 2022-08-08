1a6e851aa63c7e2d0c32c2ea1606af0acd494213
Victor Han <liyanghan1112@gmail.com>
2022-08-08 17:39:27 -0500

    added floated button to create a new tweet

---
+++ Twitter/Core/UploadTweet/Views/NewTweetView.swift
@@ -0,0 +1,51 @@
+//
+//  NewTweetView.swift
+//  Twitter
+//
+//  Created by Victor Han on 2022/8/8.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        VStack{
+            HStack{
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.systemBlue))
+                }
+                Spacer()
+                Button {
+                    print("Tweet")
+                } label: {
+                    Text("Tweet")
+                        .bold()
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
+            .padding()
+            
+            HStack(alignment:.top){
+                Circle()
+                    .frame(width:64, height:64)
+                TextArea("What's happening?", text: $caption)
+            }
+            .padding()
+        }
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
