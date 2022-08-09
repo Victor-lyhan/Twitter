817456d9c8a50db35757ffdba0dc012f0dce0be2
Victor Han <liyanghan1112@gmail.com>
2022-08-08 22:04:44 -0500

    firebase setup/integration

---
+++ Twitter/Core/Components/Tweets/RoundedShape.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedShape.swift
+//  Twitter
+//
+//  Created by Victor Han on 2022/8/8.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        return Path(path.cgPath)
+    }
+}
